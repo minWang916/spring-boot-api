2f267b80932be76cec229618b6b1c2e801f1d849
Do Minh Quang <quangdm961@gmail.com>
2024-09-09 13:06:43 +0700

    feat: add ingress for localhost

---
+++ app/src/main/java/com/kms/config/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.kms.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+  @Value("${spring.redis.host}")
+  private String redisHost;
+
+  @Value("${spring.redis.port}")
+  private int redisPort;
+
+  @Bean
+  LettuceConnectionFactory connectionFactory() {
+    RedisStandaloneConfiguration redisConfig =
+        new RedisStandaloneConfiguration(redisHost, redisPort);
+    return new LettuceConnectionFactory(redisConfig);
+  }
+
+  @Bean
+  public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+    RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(connectionFactory);
+
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+    return redisTemplate;
+  }
+}
